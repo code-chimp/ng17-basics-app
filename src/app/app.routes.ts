44a051e22e93c1ff4bfd1436aafab40a51d93d13
Tim Goshinski <tim@code-chimp.com>
2024-05-13 06:17:45 -0500

    :sparkles: routing is so easy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent,
+  },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full',
+  },
+];
