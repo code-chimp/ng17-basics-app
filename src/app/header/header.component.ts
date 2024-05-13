2b2492f61d9b165b69428799d3248ac5d2731d1b
Tim Goshinski <timothy.goshinski@kiewit.com>
2024-05-13 12:54:05 -0500

    :sparkles: Dropdown Directive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DropdownDirective } from '../directives/dropdown.directive';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, DropdownDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
